318165cdf6e72a0a7dd2dbd753d985716ea0059c
shokerm <moshe@shoker.co.il>
2023-05-04 15:26:48 +0300

    daily changes

--- src/app/services/store-data.service.ts
+++ src/app/services/store-data.service.ts
@@ -9,7 +9,7 @@ export class StoredataService {
 
   booksStore = [
     {
-      imageUrl: "../../../../assets//Images/javascript-book.jpg",
+      imageUrl: "../../../../assets//Images/store-images/javascript-book.jpg",
       imageAlt: "javascript-book",
       cardContent: "JavaScript full book",
       likeToggleStatus: "favorite_border"
@@ -17,63 +17,63 @@ export class StoredataService {
 
     },
     {
-      imageUrl: "../../../../assets//Images/entity-framework-book.jpg",
+      imageUrl: "../../../../assets//Images/store-images/entity-framework-book.jpg",
       imageAlt: "entity-framework-book",
       cardContent: "Entity Framework full book",
       likeToggleStatus: "favorite_border"
 
     },
     {
-      imageUrl: "../../../../assets//Images/jquery-book.jpg",
+      imageUrl: "../../../../assets//Images/store-images/jquery-book.jpg",
       imageAlt: "jQuery",
       cardContent: "jQuery full book",
       likeToggleStatus: "favorite_border"
 
     },
     {
-      imageUrl: "../../../../assets//Images/powershell-book.jpg",
+      imageUrl: "../../../../assets//Images/store-images/powershell-book.jpg",
       imageAlt: "Powershell",
       cardContent: "Powershell full book",
       likeToggleStatus: "favorite_border"
 
     },
     {
-      imageUrl: "../../../../assets//Images/reactjs-book.jpg",
+      imageUrl: "../../../../assets//Images/store-images/reactjs-book.jpg",
       imageAlt: "ReactJS",
       cardContent: "ReactJS full book",
       likeToggleStatus: "favorite_border"
 
     },
     {
-      imageUrl: "../../../../assets//Images/git-book.jpg",
+      imageUrl: "../../../../assets//Images/store-images/git-book.jpg",
       imageAlt: "Git",
       cardContent: "Git full book",
       likeToggleStatus: "favorite_border"
 
     },
     {
-      imageUrl: "../../../../assets//Images/ruby-on-rails-book.jpg",
+      imageUrl: "../../../../assets//Images/store-images/ruby-on-rails-book.jpg",
       imageAlt: "Ruby on Rails",
       cardContent: "Ruby on Rails full book",
       likeToggleStatus: "favorite_border"
 
     },
     {
-      imageUrl: "../../../../assets//Images/oracle-database-book.jpg",
+      imageUrl: "../../../../assets//Images/store-images/oracle-database-book.jpg",
       imageAlt: "Oracle Database",
       cardContent: "Oracle Database full book",
       likeToggleStatus: "favorite_border"
 
     },
     {
-      imageUrl: "../../../../assets//Images/ios-developer-book.jpg",
+      imageUrl: "../../../../assets//Images/store-images/ios-developer-book.jpg",
       imageAlt: "image-3",
       cardContent: "Save all your work study with you.",
       likeToggleStatus: "favorite_border",
 
     },
     {
-      imageUrl: "../../../../assets//Images/visual-basic-dotnet-book.jpg",
+      imageUrl: "../../../../assets//Images/store-images/visual-basic-dotnet-book.jpg",
       imageAlt: "Visual Basic .NET",
       cardContent: "Visual Basic .NET full book",
       likeToggleStatus: "favorite_border"
