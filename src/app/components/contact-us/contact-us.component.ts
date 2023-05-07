a1840dc53152c36276fff76029ef0d58904a0981
shokerm <moshe@shoker.co.il>
2023-05-07 13:01:55 +0300

    v

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ContactUsUserForm } from 'src/app/models.ts/contactUsUserForm';
+import { ContactUsUserForm } from 'src/app/models.ts/ContactUsUserForm';
 
 
 
