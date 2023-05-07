f331b17a96f9dfee566f3aaaa347560705782c03
shokerm <moshe@shoker.co.il>
2023-05-07 13:06:09 +0300

    models fixing changes

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ContactUsUserForm } from 'src/app/models.ts/fileContactUsUserForm';
+import { ContactUsUserForm } from 'src/app/models.ts/contactUsUserForm';
 
 
 
