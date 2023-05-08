bb3cdb959e99816fa455985f144f26186646ab8d
shokerm <moshe@shoker.co.il>
2023-05-08 14:36:05 +0300

    app module fixing

--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-import { FilterPipe } from '../pipes/filter.pipe';
-
-
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from '../app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from '../app.component';
 import { NavbarComponent } from '../components/navbar/navbar.component';
 import { FooterComponent } from '../components/footer/footer.component';
 import { AboutComponent } from '../components/about/about.component';
@@ -19,17 +16,20 @@ import { MyBooksComponent } from '../components/my-books/my-books.component';
 import { HomeComponent } from '../components/pages/home/home.component';
 import { StoreComponent } from '../components/store/store.component';
 import { AboutPageComponent } from '../components/pages/about-page/about-page.component';
-import { DataService } from '../services/data.service';
 import { CartPageComponent } from '../components/pages/cart-page/cart-page.component';
 import { MyBooksPageComponent } from '../components/pages/my-books-page/my-books-page.component';
 import { ContactUsPageComponent } from '../components/pages/contact-us-page/contact-us-page.component';
 import { StorePageComponent } from '../components/pages/store-page/store-page.component';
-import { AboutDatasService } from '../services/about-data.service';
-import { StoredataService } from '../services/store-data.service';
 import { SubNavComponent } from '../components/sub-nav/sub-nav.component';
 import { LoginDialogComponent } from '../components/dialogs-components/login-dialog-component/login-dialog.component';
 import { RegisterDialogComponent } from '../components/dialogs-components/register-dialog-component/register-dialog.component';
 import { SearchBarComponent } from '../components/search-bar/search-bar.component';
+
+import { AboutDatasService } from '../services/about-data.service';
+import { StoredataService } from '../services/store-data.service';
+import { DataService } from '../services/data.service';
+
+import { FilterPipe } from '../pipes/filter.pipe';
 
 const COMPONENTS = [ 
   AppComponent,
