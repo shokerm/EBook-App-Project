65294ac266c98c6e3aef33c9b886b670b592a052
shokerm <moshe@shoker.co.il>
2023-06-29 14:18:49 +0300

    baseUrl

--- src/app/models/baseUrl.ts
+++ src/app/models/baseUrl.ts
@@ -1,4 +1,5 @@
-// For contecting to the Azure out line 3 on comment & uncomment line 4.
+// For contecting to the Azure put line 5 on comment & uncomment line 4.
+// For contecting to the localhost put line 4 on comment & uncomment line 5.
 
 export const BASE_URL = "https://ebookapi20230627230225.azurewebsites.net/api";
 // export const BASE_URL = "https://localhost:7254/api";
